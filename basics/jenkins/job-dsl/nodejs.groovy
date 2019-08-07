159a5eda4f523a8688af6c8efb040cf318a95665
galta95 <galtaebi@gmail.com>
2019-08-07 13:59:54 +0300

    new

--- basics/jenkins/job-dsl/nodejs.groovy
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -43,3 +43,27 @@ job('NodeJS Docker example') {
         }
     }
 }
+
+pipelineJob('boilerplate-pipeline') {
+
+    def repo = "https://github.com/galta95/docker-cicd.git"
+
+    triggers {
+        scm('H/5 * * * *')
+    }
+    description("Pipeline for repo")
+    
+    definition {
+        cpsScm{
+            scm{
+                git{
+                    remote{
+                        url('git://github.com/galta95/docker-cicd.git')
+                        branches('master') 
+                        }
+                    }
+                }
+            scriptPath("/basics/misc/Jenkinsfile")
+            }
+        }
+    }
