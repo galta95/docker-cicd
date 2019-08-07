ff6384c0a0eb4ce4f4dfc493d13431b56c9f834e
galta95 <galtaebi@gmail.com>
2019-08-07 11:36:41 +0300

    new

--- basics/jenkins/job-dsl/nodejs.groovy
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -30,7 +30,7 @@ job('NodeJS Docker example') {
     wrappers {
         nodejs('node_11') 
     }
-    buildContext("./basics/jenkins/Dockerfile")
+    buildContext("./basics")
     steps {
         dockerBuildAndPublish {
             repositoryName('galta99') //qa / dev
